65decf4cf7cc414d5df647f53ffafbcabc062138
Alexander Panferov <alexandr.n.panferov@gmail.com>
2024-02-11 10:09:07 +0700

    add users

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(max_length=150, unique=True, verbose_name='почта')
+    phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)
+    city = models.CharField(max_length=50, verbose_name='Город', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
