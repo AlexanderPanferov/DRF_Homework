20d740cd9564ca685a95d011a671a9d5e337dd29
Alexander Panferov <alexandr.n.panferov@gmail.com>
2024-02-11 10:27:17 +0700

    add models course, lesson

---
+++ materials/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(upload_to='materials_course/', verbose_name='Превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='materials_lesson/', verbose_name='Превью', **NULLABLE)
+    link = models.URLField(max_length=200, verbose_name='ссылка на видео', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
